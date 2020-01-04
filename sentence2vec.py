93ef322868739fdf9fcbfa86370c8d9451320189
Dan Ofer <dan@sparkbeyond.com>
2020-01-04 15:36:45 +0200

    add FastText baseline + labelled toy train/test + sentencepiece modal + sent2vec
    Fasttext + tokenizing in ProtoPiece-FastText.ipynb
    Add train/test split toy training data
    add sentence piece model/vocab (training on 1M sequences from swissprot)
    sent2vec.py = optional baseline for sentence embedding code.

---
+++ sentence2vec.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+#
+#  Copyright 2016-2018 Peter de Vocht
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+### "A SIMPLE BUT TOUGH TO BEAT BASELINE FOR SENTENCE EMBEDDINGS" 
+### source : https://github.com/peter3125/sentence2vec/blob/master/sentence2vec.py
+
+
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD ##  dan - may be faster alternative for first principle component ?
+from typing import List
+
+# see spacy_sentence2vec.py for an example usage with real language inputs
+
+
+# an embedding word with associated vector
+class Word:
+    def __init__(self, text, vector):
+        self.text = text
+        self.vector = vector
+
+    def __str__(self):
+        return self.text + ' : ' + str(self.vector)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# a sentence, a list of words
+class Sentence:
+    def __init__(self, word_list):
+        self.word_list = word_list
+
+    # return the length of a sentence
+    def len(self) -> int:
+        return len(self.word_list)
+
+    def __str__(self):
+        word_str_list = [word.text for word in self.word_list]
+        return ' '.join(word_str_list)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# todo: get a proper word frequency for a word in a document set
+# or perhaps just a typical frequency for a word from Google's n-grams
+def get_word_frequency(word_text):
+    return 0.0001  # set to a low occurring frequency - probably not unrealistic for most words, improves vector values
+
+
+
+def sentence_to_vec(sentence_list: List[Sentence], embedding_size: int, a: float=1e-3):
+    """
+    A SIMPLE BUT TOUGH TO BEAT BASELINE FOR SENTENCE EMBEDDINGS
+    Sanjeev Arora, Yingyu Liang, Tengyu Ma
+    convert a list of sentence with word2vec items into a set of sentence vectors
+    """
+    sentence_set = []
+    for sentence in sentence_list:
+        vs = np.zeros(embedding_size)  # add all word2vec values into one vector for the sentence
+        sentence_length = sentence.len()
+        for word in sentence.word_list:
+            a_value = a / (a + get_word_frequency(word.text))  # smooth inverse frequency, SIF
+            vs = np.add(vs, np.multiply(a_value, word.vector))  # vs += sif * word_vector
+
+        vs = np.divide(vs, sentence_length)  # weighted average
+        sentence_set.append(vs)  # add to our existing re-calculated set of sentences
+
+    ## calculate PCA of this sentence set
+    # pca = PCA() ## orig
+    pca = PCA(n_components=1) ## dan - we only need first component
+    pca.fit(np.array(sentence_set))
+    u = pca.components_[0]  # the PCA vector
+    u = np.multiply(u, np.transpose(u))  # u x uT
+
+    # pad the vector?  (occurs if we have less sentences than embeddings_size)
+    if len(u) < embedding_size:
+        for i in range(embedding_size - len(u)):
+            u = np.append(u, 0)  # add needed extension for multiplication below
+
+    # resulting sentence vectors, vs = vs -u x uT x vs
+    sentence_vecs = []
+    for vs in sentence_set:
+        sub = np.multiply(u,vs)
+        sentence_vecs.append(np.subtract(vs, sub))
+
+    return sentence_vecs
